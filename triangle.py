61a997fed67e29d70e7d58849600bb791f545d2b
jeff <contact@epitez.fr>
2017-08-10 10:59:28 +0200

    Initialisation de la classe triangle

--- triangle.py
+++ triangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 
 """
 Manipulation des triangles. Détection des triangle isocèle, rectange, équilatéral et quelconque.
@@ -8,3 +9,27 @@ Manipulation des triangles. Détection des triangle isocèle, rectange, équilat
 - rectangle : pythagore a^2 = b^2 + C^2
 
 """
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class triangle:
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self, a, b, c):
+        """
+        Creation d'un triangle basé sur 3 longueurs
+        :param a: int
+        :param b: int
+        :param c: int
+        """
+        self._a = a
+        self._b = b
+        self._c = c
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __str__(self):
+        """
+        Conversion d'un triangle en chaine de caractere
+        :rtype: str
+        :return: le texte décrivant le triangle
+        """
+        return "Triangle: %s, %s, %s" % (self._a, self._b, self._c)
