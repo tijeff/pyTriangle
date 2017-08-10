9d7c41f68e18a520d983067e95cdfd636384d80b
jeff <contact@epitez.fr>
2017-08-10 11:20:11 +0200

    Ajoute traitement des triangles équilateraux

--- testTriangle.py
+++ testTriangle.py
@@ -19,7 +19,6 @@ class TestUM(unittest.TestCase):
         test_triangle = triangle.triangle(3, 4, 5)
         self.assertEqual('Triangle: 3, 4, 5', "%s" % test_triangle)
 
-
     # ------------------------------------------------------------------------------------------------------------------
     def test_isocele(self):
         test_triangle = triangle.triangle(3, 4, 3)
@@ -29,6 +28,11 @@ class TestUM(unittest.TestCase):
     def test_isocele_text(self):
         test_triangle = triangle.triangle(3, 4, 3)
         self.assertEqual(r"Triangle: 3, 4, 3 ISOCELE", test_triangle.__str__())
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test_equilateral(self):
+        test_triangle = triangle.triangle(2, 2, 2)
+        self.assertTrue(test_triangle.isEquilateral())
 
 # ----------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
